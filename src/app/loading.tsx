6bd634362cfe95b1537b253fc0ee9b745d21dcf5
Aleksandar Beric <aleksandar.beric@htecgroup.com>
2023-03-29 19:21:14 +0200

    Add loading page for home page

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center">
+        <img className="h-96" src="spinner.svg" alt="Loading..." />
+    </div>
+  )
+}
